fd3ae1a3bfd4e63c6bed1a4537b909feb6eb44ea
rashid <abdulrashid.hayato@edu.sait.ca>
2023-03-28 14:27:46 -0600

    adding supabase

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,7 @@
       <li><a href="/01-ach1"> Achievement-1 </a></li>
       <li><a href="/02-ach2/albums"> Achievement-2 </a></li>
       <li><a href="/03-ach3"> Achievement-3 </a></li>
+      <li><a href="/d04-supabase"> d04-supabase </a></li>
     
 
 
