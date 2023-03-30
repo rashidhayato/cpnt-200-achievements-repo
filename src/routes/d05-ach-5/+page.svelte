18c1ddcc08f46aaeb598de9c991e7efa06fe8c08
rashid <abdulrashid.hayato@edu.sait.ca>
2023-03-30 15:55:11 -0600

    adding achievment5

---
+++ src/routes/d05-ach-5/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let data;
+  let { rpg_item } = data;
+  console.log(rpg_item);
+</script>
+
+<h1>RPG Players</h1>
+<ul>
+  {#each rpg_item as rpg_item}
+    <li>{rpg_item.id}: {rpg_item.name}</li>
+  {/each}
+</ul>
